a3f037aa6a6fdc1fa9416e678a53ee3ae2e9b686
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2024-12-06 20:03:29 +0530

    Update server.js

---
+++ TaskManagementAPI/server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const connectDB = require('./config/database');
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to the database
+connectDB();
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
